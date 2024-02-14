cbea5b4d36db9a19578c04ce5400d2f733dcc70c
Pragyo Tandukar <84461389+tandukar@users.noreply.github.com>
2024-02-14 10:32:44 +0000

    configure project and create  models and serializers for inventory item and item purchase

--- POSMgmt/pos_mgmt/models.py
+++ POSMgmt/pos_mgmt/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
+class InventoryItem(models.Model):
+    item_code = models.CharField(max_length=10, unique=True)
+    item_name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField(default=0)  
+    quantity_unit = models.CharField(max_length=50, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) 
+    price_unit = models.CharField(max_length=50, null=True, blank=True)
+    vendor = models.CharField(max_length=255, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.item_name
+
+
+class ItemPurchase(models.Model):
+    item_code = models.CharField(max_length=10)
+    item_name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    quantity_unit = models.CharField(max_length=50, null=True, blank=True)
+    price = models.FloatField(default=0.00)
+    price_unit = models.CharField(max_length=50, null=True, blank=True) 
+    vendor = models.CharField(max_length=255, null=True, blank=True)
+    purchased_date = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.item_name
+    
+
---
+++ POSMgmt/pos_mgmt/serializers/InventorySerializer.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from ..models import *
+
+class InventoryItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = InventoryItem 
+        fields = '__all__'
+
+class ItemPurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemPurchase
+        fields = '__all__'
+
+    def create(self, validated_data):
+        item_code = validated_data.get('item_code')
+        quantity = validated_data.get('quantity')
+        vendor = validated_data.get('vendor')
+          
+        try: 
+            # Checking if InventoryItem with the itemcode and the vendor already exists
+            inventory_item = InventoryItem.objects.get(item_code=item_code, vendor=vendor)
+            inventory_item.quantity += quantity
+            inventory_item.save()
+        except InventoryItem.DoesNotExist:
+            # If it doesnt exists create a new InventoryItem instance
+            inventory_item = InventoryItem.objects.create(**validated_data)
+
+        return super().create(validated_data)
+    
+
