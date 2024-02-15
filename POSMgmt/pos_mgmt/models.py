f3fbc2666bfcb39bb187518b34c12fdc19af2a05
tandukar <84461389+tandukar@users.noreply.github.com>
2024-02-15 22:13:54 +0545

    organization model

--- POSMgmt/pos_mgmt/models.py
+++ POSMgmt/pos_mgmt/models.py
@@ -67,3 +67,15 @@ class ItemPurchase(models.Model):
 
     def __str__(self):
         return self.item_name
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=50)
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    org_email = models.EmailField()
+    org_phone = models.CharField(max_length=15)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
