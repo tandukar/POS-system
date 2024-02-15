5a79249bb6d45e5a7877443dea7d005f8834ab00
tandukar <84461389+tandukar@users.noreply.github.com>
2024-02-15 22:16:43 +0545

    login api view

--- POSMgmt/pos_mgmt/serializers/UserSerializer.py
+++ POSMgmt/pos_mgmt/serializers/UserSerializer.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from ..models import *
 
+
 class CustomUserSerializer(serializers.ModelSerializer):
     password_confirmation = serializers.CharField(max_length=128, write_only=True)
 
@@ -36,4 +37,26 @@ class CustomUserSerializer(serializers.ModelSerializer):
         validated_data.pop("password_confirmation", None)
         validated_data["password"] = make_password(validated_data.get("password"))
         return super(CustomUserSerializer, self).create(validated_data)
-    
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+
+        user = authenticate(email=email, password=password)
+
+        if user:
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            response = {
+                "access_token": access_token,
+                "refresh_token": str(refresh),
+                "user_id": user.id,
+                "email": user.email,
+            }
+
+        return response
--- POSMgmt/pos_mgmt/views.py
+++ POSMgmt/pos_mgmt/views.py
@@ -3,13 +3,12 @@ from rest_framework.views import APIView
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import CustomUser, Category, Expense, SpendingLimit
+from .models import UserProfile, InventoryItem, ItemPurchase, Organization
 from .serializers import (
     CustomUserSerializer,
     LoginSerializer,
-    CategorySerializer,
-    ExpenseSerializer,
-    SpendingLimitSerializer,
+    InventoryItemSerializer,
+    ItemPurchaseSerializer,
 )
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth import authenticate
