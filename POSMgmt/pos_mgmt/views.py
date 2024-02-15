254521ce0d867635eb13513f3a0ed5951d66dd70
tandukar <84461389+tandukar@users.noreply.github.com>
2024-02-15 22:06:49 +0545

    register api

--- POSMgmt/pos_mgmt/views.py
+++ POSMgmt/pos_mgmt/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
+from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import CustomUser, Category, Expense, SpendingLimit
+from .serializers import (
+    CustomUserSerializer,
+    LoginSerializer,
+    CategorySerializer,
+    ExpenseSerializer,
+    SpendingLimitSerializer,
+)
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+
+class RegisterUserView(APIView):
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                    "success": True,
+                    "message": "User created successfully",
+                    "data": serializer.data,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(
+            {"success": False, "error": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
